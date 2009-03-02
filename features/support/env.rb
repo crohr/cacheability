5d0461cceaf2fb770d26120632a775a36dcbf731
Cyril Rohr <cyril.rohr@irisa.fr>
2009-03-02 14:09:51 +0100

    Initial commit to cacheability.

---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'cacheability'
+
+require 'spec/expectations'
+
+require 'test/unit/assertions'
+
+World do |world|
+  
+  world
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'cacheability'
+
+Spec::Runner.configure do |config|
+  
+end
